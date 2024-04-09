a1245acafd468df6d3e123dcf4083db798d4c7cd
one1erp <avigaile@one1.co.il>
2024-04-09 09:10:15 +0300

    Initial commit with additional files

---
+++ UserControl1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Common;
+using DAL;
+using LSExtensionExplorer;
+using LSSERVICEPROVIDERLib;
+
+namespace Thermo.ExplorerDetailsExtension2
+{
+    public partial class UserControl1 : UserControl, ILSXplDetailsControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public event ExceptionThrownEventHandler ExceptionThrown;
+
+        public void PreDisplay()
+        {
+            Utils.CreateConstring(ntlCon);
+            dal = new DataLayer();
+            dal.Connect();
+        }
+
+        public void SelectionChanged(System.Collections.ArrayList[] selectionList)
+        {
+
+            var cn = selectionList[0][0].ToString();
+            var client = dal.GetClientByName(cn);
+            var q = (from item in client.Samples select item.Product.Name).Distinct();
+            listBox1.DataSource = q.ToList();
+          
+        }
+        private INautilusDBConnection ntlCon;
+
+        public void SetServiceProvider(object sp)
+        {
+            serviceProvider = (NautilusServiceProvider)sp;
+
+
+             ntlCon = Utils.GetNtlsCon(serviceProvider);
+
+
+        }
+        private void splitPanel1_Resize(object sender, EventArgs e)
+        {
+            
+        }
+        private NautilusServiceProvider serviceProvider;
+        private IDataLayer dal;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserControl1_Resize(object sender, EventArgs e)
+        {
+            label1.Location = new Point(Width / 2 - label1.Width / 2, label1.Location.Y);
+
+        }
+    }
+}
